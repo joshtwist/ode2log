118778dc0057308fad0ba20c4a973af508cc2d37
Josh Twist <joshtwist@fb.com>
2016-06-08 15:38:21 -0700

    fixing module

--- logger2.js
+++ logger2.js
@@ -1,14 +1,15 @@
 
-var config;
+var me = {
+	init : function(config) {
+		  utils.log("L2Configuration recevied");
+		  utils.log(config);
+		  this.config = config;
+	},
+	logEvent : function(message, params) {
+	  utils.log("L2 Message :" + message);
+	  utils.log("L2 Params :" + params);
+	  utils.log("L2 Config :" + config);
+	}
+};
 
-module.exports.init = function(config) {
-  utils.log("L2Configuration recevied");
-  utils.log(config);
-  config = config;
-}
-
-module.exports.logEvent = function(message, params) {
-  utils.log("L2 Message :" + message);
-  utils.log("L2 Params :" + params);
-  utils.log("L2 Config :" + config);
-}
+module.exports = me;
