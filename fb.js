3bee75657b8c6d28e02efcde1af8347af9e4ee1e
Josh Twist <joshtwist@fb.com>
2016-06-08 18:05:54 -0700

    update

--- fb.js
+++ fb.js
@@ -17,9 +17,14 @@ function FB() {
 
 	this.logEvent = function(event, params, clientParams) {
 
+		params._eventName = event; // lazy
+
 		var body = {
-			event: event,
+			event: 'CUSTOM_APP_EVENTS', // TODO - only do this if not a core event (not MOBILE_APP_INSTALL or DEFERRED_APP_LINK)
 			advertiser_id: clientParams.idfa,
+			application_tracking_enabled: 1, // TODO - actually read
+			advertiser_tracking_enabled: 1, // TODO - actually read
+			custom_events: JSON.stringify([params]);
 		}
 
 		utils.postBodyThen('https://graph.facebook.com/v2.6/' + encodeURIComponent(this.config.appid) + '/activities', objToBody(body), function(rs) {
